ba9b7b9fd2ccd6c457a2b398b730ce303e2687fd
Farid Trujillo <faridtrujillo30@gmail.com>
2025-11-24 15:31:57 -0500

    Funcionalidad basica de login

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function login(usuario, clave) {
-  if (usuario === "sancocho" && clave === "pantostado") {
+  if (usuario === "admin" && clave === "1234") {
     return "Acceso concedido";
   } else {
     return "Acceso denegado";
